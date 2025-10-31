88dfbeb834e3bc113e58b9a06b2940f46c163a9a
leiannezapioquinto <lei.pioquinto@gmail.com>
2025-10-31 16:29:17 +0800

    - Add seeders

--- database/migrations/2025_10_31_075624_create_categories_table.php
+++ database/migrations/2025_10_31_075624_create_categories_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('description');
             $table->string('color')->nullable(); // e.g., hex code or Tailwind color
             $table->timestamps();
         });
--- database/migrations/2025_10_31_075626_create_tasks_table.php
+++ database/migrations/2025_10_31_075626_create_tasks_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
             $table->foreignId('category_id')->nullable()->constrained()->onDelete('set null');
             $table->string('title');
             $table->text('description')->nullable();
